5148b9b5104c4fbe4724ecb3c780b1e521a28e34
Marcel Marques <marcmatiasdev@gmail.com>
2022-09-07 16:18:42 -0300

    Updating utils commits

--- src/utils.js
+++ src/utils.js
@@ -2,10 +2,12 @@
  * Utils functions
  */
 
+// Normalize fonts removing accents
 export function toNormalForm(str) {
   return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 }
 
+// Create new document elements
 export function newElement(element, id, classList = [], label, nodeToAppend) {
   const el = document.createElement(element);
 
